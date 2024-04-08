f0a3290cf5d110b12dce8cbd245f725894f5e0ba
Fabien Lestrat <129498777+Fables7@users.noreply.github.com>
2024-04-08 21:45:55 +0100

    Order entity

---
+++ app/(dashboard)/[storeId]/(routes)/orders/_components/Client.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { DataTable } from "@/components/ui/dataTable";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+
+import { columns, OrderColumn } from "./columns";
+
+interface OrderClientProps {
+  data: OrderColumn[];
+}
+
+export const OrderClient = ({ data }: OrderClientProps) => {
+  return (
+    <>
+      <Heading
+        title={`Orders (${data.length})`}
+        description="Manage orders for your store"
+      />
+
+      <Separator />
+      <DataTable searchKey="products" columns={columns} data={data} />
+    </>
+  );
+};
